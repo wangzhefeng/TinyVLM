7c8980da59ca8cfcc7712e302a360026ff54a58e
wangzhefeng <wangzhefengr@163.com>
2025-07-08 14:01:05 +0800

    update

--- update_repo.sh
+++ update_repo.sh
@@ -1,13 +1,19 @@
 # !/bin/bash
 
 # push TinyVLM
+epch "--------------------------"
+echo "update TinyVLM codes..."
 git add .
 git commit -m "update"
+git pull
 git push
 
 
 # push utils
+epch "--------------------------"
+echo "update utils codes..."
 cd utils
 git add .
 git commit -m "update"
+git pull
 git push
